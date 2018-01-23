6634a7b2d459cff66d773d31a5c574d7ec227200
Shy Shalom <shooshx@gmail.com>
2018-01-24 01:05:33 +0200

    fixed title of editor according to convention, propogate wtype to java

--- src/il/co/codeguru/corewars8086/gui/PlayersPanel.java
+++ src/il/co/codeguru/corewars8086/gui/PlayersPanel.java
@@ -15,12 +15,12 @@ class PlayersPanel
         TWO_DIFFERENT
     }
     public static class Code {
-        public Code(String pn, int idx) {
-            playerName = pn;
+        public Code(PlayerInfo p, int idx) {
+            player = p;
             index = idx;
         }
 
-        public String playerName; // name of the player we are in
+        public PlayerInfo player; // reference back to the player this is in
         public int index; // 0 or 1 - there are two snippets per player
         public String name; // name of this code snippet (name on the button)
         public String asmText = "";
@@ -28,13 +28,15 @@ class PlayersPanel
     }
 
     public static class PlayerInfo {
-        public PlayerInfo(String nm) {
-            name = nm;
-            code[0] = new Code(nm, 0);
-            code[1] = new Code(nm,1);
+        public PlayerInfo(String lbl, String ttl) {
+            label = lbl;
+            title = ttl;
+            code[0] = new Code(this, 0);
+            code[1] = new Code(this,1);
         }
 
-        public String name;  // A for 'Player A'
+        public String label;  // 'A', 'B' etc, the string on the elements of the player
+        public String title;  // 'Player A'
         public Code[] code = new Code[2];
         public EWarriorType wtype = EWarriorType.SINGLE;
     }
@@ -47,61 +49,99 @@ class PlayersPanel
         m_codeEditor = editor;
 
         exportMethods();
-        Console.log( findPlayer("bla") == null ? "null":"not-null");
+        //Console.log( findPlayer("bla") == null ? "null":"not-null");
     }
 
     // http://www.gwtproject.org/doc/latest/DevGuideCodingBasicsJSNI.html
     public native void exportMethods() /*-{
         var that = this
         $wnd.j_srcSelectionChanged = $entry(function(s,i) {that.@il.co.codeguru.corewars8086.gui.PlayersPanel::srcSelectionChanged(Ljava/lang/String;I)(s,i) });
-        $wnd.j_addPlayer =    $entry(function(a,b,c) { that.@il.co.codeguru.corewars8086.gui.PlayersPanel::addPlayer(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)(a,b,c) });
+        $wnd.j_addPlayer =    $entry(function(a,b) { that.@il.co.codeguru.corewars8086.gui.PlayersPanel::addPlayer(Ljava/lang/String;Ljava/lang/String;)(a,b) });
         $wnd.j_removePlayer = $entry(function(s) { that.@il.co.codeguru.corewars8086.gui.PlayersPanel::removePlayer(Ljava/lang/String;)(s) });
+        $wnd.j_changedWType = $entry(function(a,b) { that.@il.co.codeguru.corewars8086.gui.PlayersPanel::changedWType(Ljava/lang/String;Ljava/lang/String;)(a,b) });
     }-*/;
 
-    public PlayerInfo findPlayer(String name) {
+    public PlayerInfo findPlayer(String label) {
         //Console.log("###" + Integer.toString(m_players.size()));
         for(PlayerInfo p : m_players) {
-            if (p.name.equals(name))
+            if (p.label.equals(label))
                 return p;
         }
         return null;
     }
 
 
-    public void addPlayer(String name, String codeName1, String codeName2) {
-        if (name == null)
+    // from js
+    public void addPlayer(String label, String title) {
+        if (label == null)
             return;
-        if (findPlayer(name) != null)
-            throw new RuntimeException("name already taken" + name);
-        PlayerInfo p = new PlayerInfo(name);
-        p.code[0].name = codeName1;
-        p.code[1].name = codeName2;
+        if (findPlayer(label) != null)
+            throw new RuntimeException("label already taken " + label);
+        PlayerInfo p = new PlayerInfo(label, title);
+        String tu = title.replace(' ', '_');
+        p.code[0].name = tu + "1";
+        p.code[1].name = tu + "2";
         m_players.add(p);
-        Console.log("Added " + name + " " + Integer.toString(m_players.size()));
+        Console.log("Added " + label + " " + Integer.toString(m_players.size()));
     }
-    public void removePlayer(String name) {
+    // from js
+    public void removePlayer(String label) {
         for(PlayerInfo p : m_players) {
-            if (name.equals(p.name)) {
+            if (label.equals(p.label)) {
                 m_players.remove(p);
-                Console.log("Removed " + name + " " + Integer.toString(m_players.size()));
+                Console.log("Removed " + label + " " + Integer.toString(m_players.size()));
                 return;
             }
         }
-        throw new RuntimeException("player not found" + name);
+        throw new RuntimeException("player not found" + label);
     }
 
+    // from js
+    public void srcSelectionChanged(String label, int num) {
+        //Console.log("~~~~" + label + Integer.toString(num));
 
-    public void srcSelectionChanged(String name, int num) {
-        Console.log("~~~~" + name + Integer.toString(num));
-
-        PlayerInfo p = findPlayer(name);
+        PlayerInfo p = findPlayer(label);
         m_inEditor = null;
         if (p == null)
             return;
         m_inEditor = p.code[num - 1];
         m_codeEditor.setText(m_inEditor.asmText);
-        m_codeEditor.setTitle(m_inEditor.name);
+        m_codeEditor.setTitle(m_inEditor.player.title);
     }
+
+    public void changedWType(String label, String v) {
+        PlayerInfo p = findPlayer(label);
+        p.wtype = EWarriorType.valueOf(v);
+
+        reWriteButtonsLabels(p);
+    }
+
+
+    private void reWriteButtonsLabels(PlayerInfo p)
+    {
+        String vu = p.title.replace(' ', '_');
+
+        // maintain the convention for naming the warriors of a player
+        switch(p.wtype) {
+        case SINGLE:
+            p.code[0].name = vu;
+            ((HTMLElement)DomGlobal.document.getElementById("sel_code_lbl_w1_p" + p.label)).innerHTML = vu;
+            break;
+        case TWO_IDENTICAL:
+            String vup = vu + "1,2";
+            p.code[0].name = vup;
+            ((HTMLElement)DomGlobal.document.getElementById("sel_code_lbl_w1_p" + p.label)).innerHTML = vup;
+            break;
+        case TWO_DIFFERENT:
+            String vu1 = vu + "1", vu2 = vu + "2";
+            p.code[0].name = vu1;
+            p.code[1].name = vu1;
+            ((HTMLElement)DomGlobal.document.getElementById("sel_code_lbl_w1_p" + p.label)).innerHTML = vu1;
+            ((HTMLElement)DomGlobal.document.getElementById("sel_code_lbl_w2_p" + p.label)).innerHTML = vu2;
+            break;
+        }
+    }
+
 
     // from CodeEditor
     public void updateText(String text) {
@@ -113,13 +153,14 @@ class PlayersPanel
     public void updateTitle(String v) {
         if (m_inEditor == null)
             return;
-        m_inEditor.name = v;
 
-        //sel_code_lbl_w1_pA
-        String buttonId = "sel_code_lbl_w" + Integer.toString(m_inEditor.index + 1) + "_p" + m_inEditor.playerName;
-        HTMLElement buttonLabel = (HTMLElement) DomGlobal.document.getElementById(buttonId);
+        m_inEditor.player.title = v;
 
-        buttonLabel.innerHTML = v;
+        String pt = m_inEditor.player.label;
+        ((HTMLElement)DomGlobal.document.getElementById("player_name_lbl_p" + pt)).innerHTML = v;
+
+        reWriteButtonsLabels(m_inEditor.player);
+
     }
 
 }
